e76cc0db14e6be0003154d8187a94050047d0cb9
Deadbeef <ent3rm4n@gmail.com>
2022-12-07 21:54:42 +0800

    add discord token

--- shell.nix
+++ shell.nix
@@ -2,4 +2,6 @@
 with pkgs;
 mkShell {
   buildInputs = [ gcc cmake libopus ffmpeg youtube-dl ];
+  # DISCORD_TOKEN = "xxx"; # If you intend to add this token, please run `git update-index --skip-worktree shell.nix`.
+  # If you make any changes, then you may run `git update-index --no-skip-worktree shell.nix`.
 }
