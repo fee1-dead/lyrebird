68f17c5c362b7fd3cbd54e86170d6d96192df655
Deadbeef <ent3rm4n@gmail.com>
2023-01-18 09:35:01 +0800

    use youtube-dl again

--- shell.nix
+++ shell.nix
@@ -1,7 +1,8 @@
 { pkgs ? import <nixpkgs> {} }:
 with pkgs;
 mkShell {
-  buildInputs = [ gcc cmake libopus ffmpeg yt-dlp ];
+  buildInputs = [ gcc cmake libopus ffmpeg youtube-dl ];
+
   # DISCORD_TOKEN = "xxx"; # If you intend to add this token, please run `git update-index --skip-worktree shell.nix`.
   # If you make any changes, then you may run `git update-index --no-skip-worktree shell.nix`.
 }
