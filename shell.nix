c4e393d9306d53d3932c48f9bb16e27906d4c635
Deadbeef <ent3rm4n@gmail.com>
2023-01-22 14:55:45 +0800

    update nix

--- shell.nix
+++ shell.nix
@@ -1,8 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
 with pkgs;
 mkShell {
-  buildInputs = [ gcc cmake libopus ffmpeg youtube-dl ];
+  buildInputs = [ gcc cmake libopus ffmpeg yt-dlp ];
 
-  # DISCORD_TOKEN = "xxx"; # If you intend to add this token, please run `git update-index --skip-worktree shell.nix`.
-  # If you make any changes, then you may run `git update-index --no-skip-worktree shell.nix`.
+  DISCORD_TOKEN = (builtins.readFile ./discord_token);
 }
